18f847799b6737d1dc73fe596b096e06de41552b
tianjintao030 <85120907+tianjintao030@users.noreply.github.com>
2025-08-25 20:42:11 +0800

    可视化行为树节点补充

---
+++ Assets/Scripts/tjtFramework/GameAI/BehaviourTree/Nodes/InverterNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tjtFramework.GameAI.BehaviourTree
+{
+    /// <summary>
+    /// 取反器
+    /// </summary>
+    public class InverterNode : BehaviourTreeDecoratorNode
+    {
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override State OnUpdate()
+        {
+            if(child == null)
+            {
+                return State.Failure;
+            }
+
+            var result = child.Update();
+            switch(result)
+            {
+                case State.Success:
+                    return State.Failure;
+                case State.Failure:
+                    return State.Success;
+                case State.Running:
+                    return State.Running;
+                default:
+                    return State.Invalid;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/tjtFramework/GameAI/BehaviourTree/Nodes/RepeaterNode.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tjtFramework.GameAI.BehaviourTree
+{
+    /// <summary>
+    /// 重复执行器
+    /// </summary>
+    public class RepeaterNode : BehaviourTreeDecoratorNode
+    {
+        public enum RepeatMode
+        {
+            FixedCount,     // 固定次数
+            Infinite,       // 无限循环
+            UntilFailure,   // 直到失败
+            UntilSuccess    // 直到成功
+        }
+
+        public RepeatMode repeatMode;
+        [Tooltip("FixedCount模式时使用")]
+        public int repeatCount;
+        private int currentCount = 0;
+
+        protected override void OnStart()
+        {
+            currentCount = 0;
+        }
+
+        protected override void OnStop()
+        {
+            currentCount = 0;
+        }
+
+        protected override State OnUpdate()
+        {
+            if(child == null)
+            {
+                return State.Failure;
+            }
+
+            switch(repeatMode)
+            {
+                case RepeatMode.FixedCount:
+                    if(currentCount < repeatCount)
+                    {
+                        var result1 = child.Update();
+                        if(result1 == State.Failure ||  result1 == State.Success)
+                        {
+                            currentCount++;
+                            // 重置子节点以便重新运行
+                            child.Reset();
+                        }
+                        return currentCount >= repeatCount ? State.Success : State.Running;
+                    }
+                    return State.Success;
+                case RepeatMode.Infinite:
+                    var result2 = child.Update();
+                    if (result2 == State.Success || result2 == State.Failure)
+                    {
+                        child.Reset();
+                    }
+                    return State.Running;
+                case RepeatMode.UntilFailure:
+                    var result3 = child.Update();
+                    if(result3 == State.Failure)
+                    {
+                        return State.Success;
+                    }
+                    if(result3 == State.Success)
+                    {
+                        child.Reset();
+                    }
+                    return State.Running;
+                case RepeatMode.UntilSuccess:
+                    var result4 = child.Update();
+                    if (result4 == State.Success)
+                    {
+                        return State.Success;
+                    }
+                    if (result4 == State.Failure)
+                    {
+                        child.Reset();
+                    }
+                    return State.Running;
+            }
+
+            return State.Failure;
+        }
+    }
+}
+
